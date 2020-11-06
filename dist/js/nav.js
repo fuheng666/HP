e04091cb3f12158233ce143b6c5d17a788f30630
fuheng666 <1945352547@qq.com>
2020-11-06 21:21:06 +0800

    2020.11.6修改

--- dist/js/nav.js
+++ dist/js/nav.js
@@ -2,13 +2,33 @@ define(['jquery'],function(){
         console.log('nav加载成功')
         var triangle=$('.triangle')
         var divList=$('.divList')
-        var a=$('#ui-id-1').find('li').find('a')
+        var a=$('.level0')
         function pull(){
             console.log(a)
-           a.click(function(){
-               console.log('我点击了')
+           a.on('mouseenter',function(){
+               var a =$(this).index()
+               divList.eq(a).css('display','flex')
+               triangle.eq(a).css('display','block')
            })
+           divList.on('mouseenter',function(){
+            var a =$(this).index()
+            divList.eq(a).css('display','flex')
+            triangle.eq(a).css('display','block')
+        })
+        
+        divList.on('mouseleave',function(){
+            var a =$(this).index()
+            divList.eq(a).css('display','none')
+            triangle.eq(a).css('display','none')
+        })
+        a.on('mouseleave',function(){
+            var a =$(this).index()
+            divList.eq(a).css('display','none')
+            triangle.eq(a).css('display','none')
+        })
         }
+      
+        console.log(divList)
         return{
             pull
         }
