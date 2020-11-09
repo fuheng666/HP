ccdc8e4c72dddb81324fb20f97be8681a74b1b16
fuheng666 <1945352547@qq.com>
2020-11-09 12:50:28 +0800

    2020.11.9中午

---
+++ dist/js/trade.js
@@ -0,0 +1,8 @@
+define(['jquery'],function(){
+    function shop() {
+        console.log('加载成功');
+    }
+    return{
+        shop
+    }
+})
---
+++ js/productList.js
@@ -0,0 +1,32 @@
+/*
+  配置要引入的模块的路径 jquery也遵从AMD规范
+*/
+require.config({
+    paths: {
+      jquery: "jquery-1.11.3",
+      "jquery-cookie": "jquery.cookie",
+      parabola: "parabola",
+      trade:'trade',
+      banner: "banner",
+      nav:'nav'
+    },
+    //jquery-cookie 是依赖于jquery开发
+    shim: {
+      //设置依赖关系
+      "jquery-cookie": ["jquery"],
+      //parabola 不遵从AMD规范
+      parabola: {
+        exports: "_",
+      },
+    },
+  });
+  
+  
+  /*
+    好处：模块和模块之间的关系清晰，所以的代码的，其中一个模块有问题，其他代码不受影响。
+  */
+  require(['trade','banner','nav'], function(trade,banner,nav){
+    trade.shop();
+    nav.pull();
+  })
+  
